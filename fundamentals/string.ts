9bcfba51f2921911a7db5d4ee229b0bcce2aba45
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-08-10 14:47:40 +0530

    string basics

---
+++ fundamentals/string.ts
@@ -0,0 +1,13 @@
+const str: string = "nabhag";
+const str1: string = "nabhag";
+
+console.log(str.charAt(1));
+console.log(str.at(1));
+console.log(str.includes("sdfds"));
+console.log(str.replaceAll("a", "i"));
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.concat("nabhag"));
+console.log(str.startsWith("abha", 1));
+console.log(str === str1);
