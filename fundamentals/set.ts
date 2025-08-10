a7d79958b1f76866b36b438192e86fc56c6fd197
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-08-10 15:19:12 +0530

    set

--- fundamentals/set.ts
+++ fundamentals/set.ts
@@ -1,3 +1,13 @@
 const set = new Set<number>([1, 2, 4, 55, 6, 5, 4, 4, 3, 3, 3, 3, 3, 3]);
-set.add(23423);
+console.log(set.add(23423));
+console.log(set.delete(1));
+console.log(set.size);
+console.log(set.has(1));
 console.log(set);
+
+for (const [key, value] of set.entries()) {
+  console.log([key, value]);
+}
+
+// for(const value of set.keys())
+// for(const value of set.values()) //both works same
