eed2769270fcc0adb5661c805ebc6143d4b92877
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-08-13 15:00:21 +0530

    linkedlist

--- fundamentals/linkedlist.ts
+++ fundamentals/linkedlist.ts
@@ -32,6 +32,89 @@ class LinkedList<T> {
     this.size++;
   }
 
+  // 1 -> 2 -> 3 -> 4 -> 5
+
+  insertAt(index: number, data: T): void {
+    if (index < 0 || index >= this.size) {
+      return;
+    }
+
+    if (index === 0) {
+      this.prepend(data);
+    } else {
+      const node = new ListNode<T>(data);
+      let first = this.head;
+
+      for (let i = 0; i < index - 1; ++i) {
+        first = first.next;
+      }
+
+      const second = first.next;
+      first.next = node;
+      node.next = second;
+    }
+
+    this.size++;
+  }
+
+  removeAt(index: number): T | null {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    if (index === 0) {
+      return this.removeFirst();
+    }
+
+    if (index + 1 === this.size) {
+      return this.removeLast();
+    }
+
+    let current = this.head;
+
+    for (let i = 0; i < index - 1; ++i) {
+      current = current.next;
+    }
+
+    const value = current.next.data;
+    current.next = current.next.next;
+
+    --this.size;
+    return value;
+  }
+
+  removeLast(): T | null {
+    if (!this.head) {
+      return null;
+    }
+
+    if (this.getSize() === 1) {
+      const value = this.head.data;
+      this.head = null;
+      return value;
+    }
+
+    let current = this.head;
+
+    while (current.next && current.next.next !== null) {
+      current = current.next;
+    }
+
+    const data = current.next.data;
+    current.next = null;
+    --this.size;
+    return data;
+  }
+
+  removeFirst(): T | null {
+    if (!this.head) return null;
+
+    const data = this.head.data;
+    this.head = this.head.next;
+    --this.size;
+    return data;
+  }
+
   append(data: T): void {
     const node = new ListNode<T>(data);
 
@@ -70,8 +153,22 @@ class LinkedList<T> {
 }
 
 const list = new LinkedList<number>();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
 list.append(5);
-list.append(10);
+list.append(6);
+list.append(7);
 console.log(list.getSize());
+list.insertAt(6, 0);
+list.display();
 
+list.removeFirst();
+list.display();
+
+list.removeLast();
+list.display();
+
+list.removeAt(100);
 list.display();
